b38f4980cb76320ec0bf83d1a535cdce6ef84921
saujanyastha <52270311+saujanyastha@users.noreply.github.com>
2026-03-27 21:28:45 +0545

    feat: add a comprehensive navigation bar with dashboard, rentals, vehicles, payments, and a management dropdown for administrative links.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,13 +2,13 @@
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center mt-6">
                     <a href="{{ route('dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
-                <div class="hidden space-x-8 sm:ms-10 sm:flex">
+                <div class="hidden mt-6 space-x-8 sm:ms-10 sm:flex">
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
@@ -29,14 +29,14 @@
                     <x-dropdown align="left" width="48">
                         <x-slot name="trigger">
                             {{-- MODIFIED BUTTON CLASSES BELOW --}}
-                            <button class="inline-flex items-center px-1 pt-1 h-full text-sm font-medium leading-5 transition duration-150 ease-in-out
+                            <button class="inline-flex items-center px-1 pt-3 mt-3 h-full text-sm font-medium leading-5 transition duration-150 ease-in-out
                                 {{ request()->routeIs(['users.*', 'user_types.*', 'vehicle_types.*', 'payment_methods.*', 'payment_types.*'])
                                     ? 'text-gray-900 border-b-2 border-indigo-400 focus:outline-none focus:border-indigo-700'
                                     : 'text-gray-500 hover:text-gray-700 hover:border-gray-300 focus:outline-none focus:text-gray-700 focus:border-gray-300' }}
                             ">
-                                <div >{{ __('Management') }}</div>
+                                <div>{{ __('Management') }}</div>
                                 <div class="ms-1">
-                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="20 20 20 20">
                                         <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
                                     </svg>
                                 </div>
